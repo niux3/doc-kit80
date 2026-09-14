5cfb8b7ec41c50bb9e4c3e26b552654116fdd2ec
niux3 <renaudbourdeau@gmail.com>
2026-09-14 18:53:47 +0200

    feat: reogranize folder src

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     test: {
         environment: 'happy-dom', // Active la simulation de l'objet window
         globals: true
-    }
+    },
+    build: {
+        rollupOptions: {
+            input: resolve(__dirname, 'src/js/main.js'),
+        },
+    },
 })
