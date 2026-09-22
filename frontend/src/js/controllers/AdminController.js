3c77665da8221797d999aaf6ef19db00e17e46e9
niux3 <renaudbourdeau@gmail.com>
2026-09-22 17:06:52 +0200

    feat: basic form for crud

--- frontend/src/js/controllers/AdminController.js
+++ frontend/src/js/controllers/AdminController.js
@@ -3,6 +3,20 @@ import { AppAdminController } from "./AppAdminController"
 
 export default class AdminController extends AppAdminController {
 
+    init() {
+        this.categories = [
+            { id: 1, name: 'Category 1' },
+            { id: 2, name: 'Category 2' },
+            { id: 3, name: 'Category 3' },
+            { id: 4, name: 'Category 4' },
+            { id: 5, name: 'Category 5' },
+        ]
+        this.languages = [
+            { id: 1, name: 'English' },
+            { id: 2, name: 'French' },
+        ]
+    }
+
     async admin_home(req) {
         if (req.method === 'POST') {
             console.log('AdminController > admin_home > ', req.body)
@@ -21,9 +35,24 @@ export default class AdminController extends AppAdminController {
                 { id: 1, name: 'English', abbr: 'en' },
                 { id: 2, name: 'French', abbr: 'fr' },
             ],
-            link_add_name: 'admin_language',
+            link_add_name: 'admin_language_add',
+            link_text: "Ajouter une langue",
         }
         return this.render('admin/home_gridview', ctx)
+    }
+
+    async admin_language_add(req) {
+        this.setTitle("Éditer une langue")
+        const ctx = {
+            title: this.getTitle(),
+            formtype: 'form_languages',
+        }
+        if (req.method === 'POST') {
+            console.log('AdminController > admin_language_add > ', req.body)
+            console.log('AdminController >admin_language_add > POST > ', req.method)
+            return this.redirect(this.urlFor('admin_language'))
+        }
+        return this.render('admin/edit', ctx)
     }
 
     async admin_category(req) {
@@ -32,9 +61,26 @@ export default class AdminController extends AppAdminController {
             title: this.getTitle(),
             fields: [],
             data: [],
-            link_add_name: 'admin_language',
+            link_add_name: 'admin_category_add',
+            link_text: "Ajouter une catégorie",
         }
         return this.render('admin/home_gridview', ctx)
+    }
+
+    async admin_category_add(req) {
+        this.setTitle("Éditer une catégorie")
+        const ctx = {
+            title: this.getTitle(),
+            formtype: 'form_categories',
+            languages: this.languages,
+            categories: this.categories,
+        }
+        if (req.method === 'POST') {
+            console.log('AdminController > admin_category_add > ', req.body)
+            console.log('AdminController >admin_category_add > POST > ', req.method)
+            return this.redirect(this.urlFor('admin_category'))
+        }
+        return this.render('admin/edit', ctx)
     }
 
     async admin_post(req) {
@@ -43,8 +89,26 @@ export default class AdminController extends AppAdminController {
             title: this.getTitle(),
             fields: [],
             data: [],
-            link_add_name: 'admin_language',
+            link_add_name: 'admin_post_add',
+            link_text: "Ajouter un article",
         }
         return this.render('admin/home_gridview', ctx)
+    }
+
+    async admin_post_add(req) {
+        this.setTitle("Éditer un article")
+        const ctx = {
+            title: this.getTitle(),
+            formtype: 'form_posts',
+            languages: this.languages,
+            categories: this.categories,
+        }
+        if (req.method === 'POST') {
+            console.log('AdminController > admin_post_add > ', req.body)
+            console.log('AdminController >admin_post_add > POST > ', req.method)
+            return this.redirect(this.urlFor('admin_post'))
+        }
+        return this.render('admin/edit', ctx)
+
     }
 }
--- frontend/src/js/routes.js
+++ frontend/src/js/routes.js
@@ -2,7 +2,13 @@ export default [
     { path: '/:lang', action: 'home@PagesController', name: 'home', params: { lang: /[a-z]{2}/ } },
     { path: '/', action: 'home@PagesController', name: 'home' },
     { path: '/admin', action: 'admin_home@AdminController', name: 'admin_home' },
+
     { path: '/admin/language', action: 'admin_language@AdminController', name: 'admin_language' },
+    { path: '/admin/language/add', action: 'admin_language_add@AdminController', name: 'admin_language_add' },
+
     { path: '/admin/category', action: 'admin_category@AdminController', name: 'admin_category' },
+    { path: '/admin/category/add', action: 'admin_category_add@AdminController', name: 'admin_category_add' },
+
     { path: '/admin/post', action: 'admin_post@AdminController', name: 'admin_post' },
+    { path: '/admin/post/add', action: 'admin_post_add@AdminController', name: 'admin_post_add' },
 ]
