b08a710516796ff594ff4ae02a6deaa192b051d5
niux3 <renaudbourdeau@gmail.com>
2026-09-23 14:53:12 +0200

    feat: change route name and method name

--- frontend/src/js/routes.js
+++ frontend/src/js/routes.js
@@ -4,11 +4,11 @@ export default [
     { path: '/admin', action: 'admin_home@AdminController', name: 'admin_home' },
 
     { path: '/admin/language', action: 'admin_language@AdminController', name: 'admin_language' },
-    { path: '/admin/language/add', action: 'admin_language_add@AdminController', name: 'admin_language_add' },
+    { path: '/admin/language/edit/:id', action: 'admin_language_edit@AdminController', name: 'admin_language_edit', params: { id: /[0-9]*/ } },
 
     { path: '/admin/category', action: 'admin_category@AdminController', name: 'admin_category' },
-    { path: '/admin/category/add', action: 'admin_category_add@AdminController', name: 'admin_category_add' },
+    { path: '/admin/category/edit/:id', action: 'admin_category_edit@AdminController', name: 'admin_category_edit', params: { id: /[0-9]*/ } },
 
     { path: '/admin/post', action: 'admin_post@AdminController', name: 'admin_post' },
-    { path: '/admin/post/add', action: 'admin_post_add@AdminController', name: 'admin_post_add' },
+    { path: '/admin/post/edit/:id', action: 'admin_post_edit@AdminController', name: 'admin_post_edit', params: { id: /[0-9]*/ } },
 ]
