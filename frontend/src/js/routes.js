378458515d560781903f82aa6462e19bcfe38f43
niux3 <renaudbourdeau@gmail.com>
2026-09-23 19:37:39 +0200

    feat: add route delete category

--- frontend/src/js/routes.js
+++ frontend/src/js/routes.js
@@ -9,6 +9,7 @@ export default [
 
     { path: '/admin/category', action: 'admin_category@AdminController', name: 'admin_category' },
     { path: '/admin/category/edit/:id', action: 'admin_category_edit@AdminController', name: 'admin_category_edit', params: { id: /[0-9]*/ } },
+    { path: '/admin/category/delete/:id', action: 'admin_category_delete@AdminController', name: 'admin_category_delete', params: { id: /[0-9]+/ } },
 
     { path: '/admin/post', action: 'admin_post@AdminController', name: 'admin_post' },
     { path: '/admin/post/edit/:id', action: 'admin_post_edit@AdminController', name: 'admin_post_edit', params: { id: /[0-9]*/ } },
